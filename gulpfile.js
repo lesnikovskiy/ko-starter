13f257844b0a74a703bf20185cc9dece21c4b8f6
lesnikovskiy <lesnikovski@gmail.com>
2015-11-20 18:06:10 +0200

    add server

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var browserify = require("browserify");
 var stringify = require("stringify");
 var babelify = require("babelify");
 var source = require("vinyl-source-stream");
+var exec = require("child_process").exec;
+
+gulp.task("server", function(cb) {
+	return exec("node server.js", function(err, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		cb(err);
+	});
+});
 
 gulp.task("build", function() {
 	return browserify({ entries: "./src/app.js" })
@@ -20,4 +29,4 @@ gulp.task("watch", function() {
 	});
 });
 
-gulp.task("default", ["watch"]);
+gulp.task("default", ["server", "watch"]);
